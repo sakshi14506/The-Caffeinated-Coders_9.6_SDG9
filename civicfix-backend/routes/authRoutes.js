834eeab89c5b898a1b1b43418bfe1ac331ba6682
SakshiRakhade <sakshirakhade.ml24@sbjit.edu.in>
2026-01-31 02:29:10 +0530

    authadded

---
+++ civicfix-backend/routes/authRoutes.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const router = express.Router();
+
+/* REGISTER */
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).json({ msg: "User already exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    user = new User({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    await user.save();
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d"
+    });
+
+    res.json({ token });
+  } catch (err) {
+    res.status(500).send("Server error");
+  }
+});
+/* LOGIN */
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ msg: "Invalid credentials" });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials" });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d"
+    });
+
+    res.json({ token });
+  } catch (err) {
+    res.status(500).send("Server error");
+  }
+});
+
+module.exports = router;
+
--- server.js
+++ server.js
@@ -1,34 +1,20 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
-const path = require("path");
+const connectDB = require("./config/db");
 require("dotenv").config();
-
-const complaintRoutes = require("./routes/complaintRoutes");
 
 const app = express();
 
-// CORS FULL OPEN
-app.use(cors({ origin: "*" }));
+connectDB();
 
+app.use(cors());
 app.use(express.json());
 
-// serve uploaded images
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/api/auth", require("./routes/authRoutes"));
 
-// routes
-app.use("/complaints", complaintRoutes);
-
-// test
 app.get("/", (req, res) => {
-  res.send("CivicFix Backend Running");
+  res.send("Auth Backend Running");
 });
 
-// mongo connect
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected successfully ✅"))
-  .catch(err => console.error(err));
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
